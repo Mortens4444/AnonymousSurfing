874f69bdacfce46a48b4f1fb9124309ebd88df8f
Mortens <mortens.4444@gmail.com>
2024-10-05 20:01:00 +0200

    Initial commit

---
+++ AnonymousSurfing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+var proxyAddress = "178.48.68.61:18080"; // Sopron
+
+var httpClientHandler = new HttpClientHandler
+{
+    Proxy = new WebProxy(proxyAddress, true),
+    UseProxy = true,
+    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+};
+
+using var httpClient = new HttpClient(httpClientHandler)
+{
+    Timeout = TimeSpan.FromSeconds(120)
+};
+var response = await httpClient.GetStringAsync("https://browserleaks.com/ip");
+var cityPattern = "<tr><td>City</td><td>";
+var index = response.IndexOf(cityPattern) + cityPattern.Length;
+Console.WriteLine($"City: {response.Substring(index, 30)}");
+Console.ReadKey();
